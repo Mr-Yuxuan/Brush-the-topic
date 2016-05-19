ce917e156a8b192aec179669b694b0add115af52
Mr-Yuxuan <945798300@qq.com>
2016-05-19 14:18:11 +0800

    找环入口点

---
+++ 找环的入口点/找环的入口点/FindBeginning.cpp
@@ -0,0 +1,33 @@
+  //找出链表的环入口点
+ListNode* FindBeginning(ListNode* head)
+{
+	if (head == NULL)
+		return NULL;
+	ListNode* fast = head;
+	ListNode* slow = head;
+
+	//find meeting point  
+	while (fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			break;
+		}
+	}
+
+	// check iscricle
+	if (fast->next == NULL)
+	{
+		return NULL;
+	}
+    slow = head;
+	while (slow!= fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	
+	return fast;
+}
